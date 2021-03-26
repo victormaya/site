26740bf6e5667c18916ce3f8fcb2a437f27c711a
Victor Maya <victor.maya42@gmail.com>
2021-03-25 21:47:35 -0300

    mobile feito

--- script.js
+++ script.js
@@ -1,125 +1,161 @@
-// fixar navbar
-const nav = document.querySelector(".navbar");
-const ghost = document.querySelector(".navbar-ghost");
-const topoNav = nav.offsetTop;
+if (window.innerWidth > 900) {
+  // fixar navbar
+  const nav = document.querySelector(".navbar");
+  const ghost = document.querySelector(".navbar-ghost");
+  const topoNav = nav.offsetTop;
 
-function navFixed() {
-  if (window.pageYOffset >= topoNav) {
-    nav.classList.add("navbar-fixed");
-    ghost.classList.add("navbar-ghost-ativo");
-  } else {
-    nav.classList.remove("navbar-fixed");
-    ghost.classList.remove("navbar-ghost-ativo");
+  function navFixed() {
+    if (window.pageYOffset >= topoNav) {
+      nav.classList.add("navbar-fixed");
+      ghost.classList.add("navbar-ghost-ativo");
+    } else {
+      nav.classList.remove("navbar-fixed");
+      ghost.classList.remove("navbar-ghost-ativo");
+    }
   }
-}
 
-// capiturando itens do navbar
-const perfil = document.querySelector("#perfil");
-const skills = document.querySelector("#skills");
-const educacao = document.querySelector("#educacao");
-const experiencia = document.querySelector("#experiencia");
-const portfolio = document.querySelector("#portfolio");
-const contato = document.querySelector("#contato");
-const itemsAll = document.querySelectorAll(".item-navbar");
+  // capiturando itens do navbar
+  const perfil = document.querySelector("#perfil");
+  const skills = document.querySelector("#skills");
+  const educacao = document.querySelector("#educacao");
+  const experiencia = document.querySelector("#experiencia");
+  const portfolio = document.querySelector("#portfolio");
+  const contato = document.querySelector("#contato");
+  const itemsAll = document.querySelectorAll(".item-navbar");
 
-function ativarMenu() {
-  const pagetop = window.pageYOffset;
-  console.log(pagetop);
-  if (pagetop < 377) {
-    itemsAll.forEach((item) => {
-      item.classList.remove("item-navbar-ativo");
-    });
-    perfil.classList.add("item-navbar-ativo");
+  function ativarMenu() {
+    const pagetop = window.pageYOffset;
+    console.log(pagetop);
+    if (pagetop < 377) {
+      itemsAll.forEach((item) => {
+        item.classList.remove("item-navbar-ativo");
+      });
+      perfil.classList.add("item-navbar-ativo");
+    }
+    if (pagetop >= 377 && pagetop < 805) {
+      itemsAll.forEach((item) => {
+        item.classList.remove("item-navbar-ativo");
+      });
+      skills.classList.add("item-navbar-ativo");
+    } else if (pagetop >= 805 && pagetop < 1116) {
+      itemsAll.forEach((item) => {
+        item.classList.remove("item-navbar-ativo");
+      });
+      educacao.classList.add("item-navbar-ativo");
+    } else if (pagetop >= 1116 && pagetop < 1514) {
+      itemsAll.forEach((item) => {
+        item.classList.remove("item-navbar-ativo");
+      });
+      experiencia.classList.add("item-navbar-ativo");
+    } else if (pagetop >= 1514) {
+      itemsAll.forEach((item) => {
+        item.classList.remove("item-navbar-ativo");
+      });
+      portfolio.classList.add("item-navbar-ativo");
+      contato.classList.add("item-navbar-ativo");
+    }
   }
-  if (pagetop >= 377 && pagetop < 805) {
-    itemsAll.forEach((item) => {
-      item.classList.remove("item-navbar-ativo");
-    });
-    skills.classList.add("item-navbar-ativo");
-  } else if (pagetop >= 805 && pagetop < 1116) {
-    itemsAll.forEach((item) => {
-      item.classList.remove("item-navbar-ativo");
-    });
-    educacao.classList.add("item-navbar-ativo");
-  } else if (pagetop >= 1116 && pagetop < 1514) {
-    itemsAll.forEach((item) => {
-      item.classList.remove("item-navbar-ativo");
-    });
-    experiencia.classList.add("item-navbar-ativo");
-  } else if (pagetop >= 1514) {
-    itemsAll.forEach((item) => {
-      item.classList.remove("item-navbar-ativo");
-    });
-    portfolio.classList.add("item-navbar-ativo");
-    contato.classList.add("item-navbar-ativo");
-  }
-}
 
-const listaNav = nav.querySelectorAll(".item-navbar");
+  const listaNav = nav.querySelectorAll(".item-navbar");
 
-const goScroll = (index) => {
-  switch (index) {
-    case 0:
-      window.scrollTo(0, 0);
-      break;
-    case 1:
-      window.scrollTo(0, 377);
-      break;
-    case 2:
-      window.scrollTo(0, 842);
-      break;
-    case 3:
-      window.scrollTo(0, 1168);
-      break;
-    case 4:
-      window.scrollTo(0, 1577);
-      break;
-    case 5:
-      window.scrollTo(0, 1577);
-      break;
-    case 6:
-      break;
-  }
-};
+  const goScroll = (index) => {
+    switch (index) {
+      case 0:
+        window.scrollTo(0, 0);
+        break;
+      case 1:
+        window.scrollTo(0, 377);
+        break;
+      case 2:
+        window.scrollTo(0, 842);
+        break;
+      case 3:
+        window.scrollTo(0, 1168);
+        break;
+      case 4:
+        window.scrollTo(0, 1577);
+        break;
+      case 5:
+        window.scrollTo(0, 1577);
+        break;
+      case 6:
+        break;
+    }
+  };
 
-listaNav.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    goScroll(index);
+  listaNav.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      goScroll(index);
+    });
   });
-});
 
-const sections = document.querySelectorAll(".sessao");
-const separadores = document.querySelectorAll('.separador')
-console.log(separadores)
+  const sections = document.querySelectorAll(".sessao");
+  const separadores = document.querySelectorAll(".separador");
+  console.log(separadores);
 
-const scrollSection = () => {
-  const pagetop = window.pageYOffset;
-  console.log(pagetop);
-  if (pagetop >= 0) {
-    sections[0].classList.add('sessao-ativo')
-    separadores[0].classList.add('separador-ativo')
-    separadores[1].classList.add('separador-ativo')
-  }
-  if (pagetop >= 66) {
-    sections[1].classList.add('sessao-ativo')
-    separadores[2].classList.add('separador-ativo')
-  }
-  if (pagetop >= 452) {
-    sections[2].classList.add('sessao-ativo')
-    separadores[3].classList.add('separador-ativo')
-  }
-  if (pagetop >= 959) {
-    sections[3].classList.add('sessao-ativo')
-    separadores[4].classList.add('separador-ativo')
-  }
-  if (pagetop >= 1444) {
-    sections[4].classList.add('sessao-ativo')
-    separadores[5].classList.add('separador-ativo')
-  }
-};
+  const scrollSection = () => {
+    const pagetop = window.pageYOffset;
+    console.log(pagetop);
+    if (pagetop >= 0) {
+      sections[0].classList.add("sessao-ativo");
+      separadores[0].classList.add("separador-ativo");
+      separadores[1].classList.add("separador-ativo");
+    }
+    if (pagetop >= 66) {
+      sections[1].classList.add("sessao-ativo");
+      separadores[2].classList.add("separador-ativo");
+    }
+    if (pagetop >= 452) {
+      sections[2].classList.add("sessao-ativo");
+      separadores[3].classList.add("separador-ativo");
+    }
+    if (pagetop >= 959) {
+      sections[3].classList.add("sessao-ativo");
+      separadores[4].classList.add("separador-ativo");
+    }
+    if (pagetop >= 1444) {
+      sections[4].classList.add("sessao-ativo");
+      separadores[5].classList.add("separador-ativo");
+    }
+  };
 
-window.onscroll = () => {
-  navFixed();
-  ativarMenu();
-  scrollSection();
-};
+  window.onscroll = () => {
+    navFixed();
+    ativarMenu();
+    scrollSection();
+  };
+} else{
+  const sections = document.querySelectorAll(".sessao");
+  const separadores = document.querySelectorAll(".separador");
+  console.log(separadores);
+
+  const scrollSection = () => {
+    const pagetop = window.pageYOffset;
+    console.log(pagetop);
+    if (pagetop >= 159) {
+      sections[0].classList.add("sessao-ativo");
+      separadores[0].classList.add("separador-ativo");
+      separadores[1].classList.add("separador-ativo");
+    }
+    if (pagetop >= 424) {
+      sections[1].classList.add("sessao-ativo");
+      separadores[2].classList.add("separador-ativo");
+    }
+    if (pagetop >= 844) {
+      sections[2].classList.add("sessao-ativo");
+      separadores[3].classList.add("separador-ativo");
+    }
+    if (pagetop >= 1162) {
+      sections[3].classList.add("sessao-ativo");
+      separadores[4].classList.add("separador-ativo");
+    }
+    if (pagetop >= 1798) {
+      sections[4].classList.add("sessao-ativo");
+      separadores[5].classList.add("separador-ativo");
+    }
+  };
+
+  window.onscroll = () => {
+    scrollSection();
+  };
+}
